2de651df1e699b347b198de4f38599edd1ab2389
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-07 22:46:58 +0800

    Update 标准.js

--- 1.two-sum/标准.js
+++ 1.two-sum/标准.js
@@ -2,14 +2,14 @@
 //@param {number} target
 //@return {number[]}
 
-var twoSum=function(nums,target){
-	for(var i=0;i<nums.length;i++){
-		var s=target-nums[i];
-		var temp=nums.indexOf(s);
-		if(~temp&&i!==temp){
-			return [i,temp];
+var twoSum = function(nums, target) {
+	for(var i = 0; i < nums.length; i++) {
+		var s = target - nums[i];
+		var temp = nums.indexOf(s);
+		if(~temp && i !== temp) {
+			return [i, temp];
 		}
 	}
 };
-var nums=[2,7,11,15];
-console.log(twoSum(nums,9);
+var nums = [2, 7, 11, 15];
+console.log(twoSum(nums, 9));

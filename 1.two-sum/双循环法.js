2e1c060975a0ded3982c7b53b3a345b946a9b0e2
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-07 22:34:15 +0800

    Add files via upload

---
+++ 1.two-sum/双循环法.js
@@ -0,0 +1,11 @@
+//var nums=[];
+var  twoSum(function(nums,target){
+	for(int i=0;i<nums.length;i++){
+		for(int j=i+1;j<length;j++){
+			if(nums[i]+nums[j]==target){
+				return  i,j;
+			}
+		}
+	}
+};
+console.log(twoSum({2,7,11,15},9))
---
+++ 1.two-sum/标准.js
@@ -0,0 +1,15 @@
+//@param {number[]} nums
+//@param {number} target
+//@return {number[]}
+
+var twoSum=function(nums,target){
+	for(var i=0;i<nums.length;i++){
+		var s=target-nums[i];
+		var temp=nums.indexOf(s);
+		if(~temp&&i!==temp){
+			return [i,temp];
+		}
+	}
+};
+var nums=[2,7,11,15];
+console.log(twoSum(nums,9);

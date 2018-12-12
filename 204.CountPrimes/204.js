c665ae777b067de3531ec2d07078dd65298eba7f
晓樱花开 <40104942+13213802563@users.noreply.github.com>
2018-12-12 22:22:51 +0800

    Update 204.js

--- 204.CountPrimes/204.js
+++ 204.CountPrimes/204.js
@@ -1,32 +1,34 @@
 @param {number} n
 @return {number}
 
-var countPrimes=function(n){
-	if(n<=2){
-		return 0;
-	}
-	if(n===3){
-		return 1;
-	}
-	var i=3;
-	var j=2;
-	var res=1;
-	var flag;//标志数
-	while(i<n){
-		j=2;
-		flag=0;
-		while(j=Math.ceil(Math.sqrt(i))){//i=10,j=4
-			if(i%j===0){
-				flag=1;
-				break;
-			}
-			j++;
-		}
-		if(!flag){
-			res++;
-		}
-		i=i+2;
-	}
-	return res;
-}
+var countPrimes = function (n) {
+  if(n <= 2) {
+    return 0;
+  }
+  if(n === 3) {
+    return 1;
+  }
+  let i = 3;
+  let j = 2;
+  let res = 1;
+  let flag;
+  while (i < n) {
+    j = 2;
+    flag = 0;
+    while (j <= Math.ceil(Math.sqrt(i))) {
+      if(i % j === 0) {
+        flag = 1;
+        break;
+      }
+     j++;
+    }
+    console.log(i);
+    console.log(flag);
+   if(!flag) {
+      res++;
+    }
+    i = i + 2;
+  }
+  return res;
+};
 console.log(countPrimes(10));
